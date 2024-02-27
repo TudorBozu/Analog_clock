e98938ea9fba38cfd22ece9934087e60dfe58ffa
Bozu Tudor <bozutudor6@gmail.com>
2024-02-27 17:17:51 +0200

    Add new screnn and add navigation bettwen they

---
+++ app/src/main/java/com/example/analog_clock/DigitalScrenn.java
@@ -0,0 +1,14 @@
+package com.example.analog_clock;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DigitalScrenn extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_digital_screnn);
+    }
+}
--- app/src/main/java/com/example/analog_clock/MainActivity.java
+++ app/src/main/java/com/example/analog_clock/MainActivity.java
@@ -2,9 +2,11 @@ package com.example.analog_clock;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AnalogClock;
+import android.widget.Button;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
@@ -14,13 +16,21 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
          AnalogClock analogclock = findViewById(R.id.analog_clock);
-
+         Button next_screnn = findViewById(R.id.next_screnn);
         analogclock.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Toast.makeText(MainActivity.this, "Analog Clock",
                         Toast.LENGTH_SHORT).show();
             }});
+           next_screnn.setOnClickListener(new View.OnClickListener() {
+               @Override
+               public void onClick(View v) {
+                   Intent intent = new Intent(MainActivity.this, DigitalScrenn.class);
+                   startActivity(intent);
+               }
+           });
+
     }
 
 
