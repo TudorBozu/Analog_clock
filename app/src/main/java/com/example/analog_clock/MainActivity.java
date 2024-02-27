8ec371c9791bde2716b4c8b2da10fa0a09c90ff2
Bozu Tudor <bozutudor6@gmail.com>
2024-02-27 16:52:49 +0200

    Crate analog clock

--- app/src/main/java/com/example/analog_clock/MainActivity.java
+++ app/src/main/java/com/example/analog_clock/MainActivity.java
@@ -3,6 +3,9 @@ package com.example.analog_clock;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AnalogClock;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+         AnalogClock analogclock = findViewById(R.id.analog_clock);
+
+        analogclock.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "Analog Clock",
+                        Toast.LENGTH_SHORT).show();
+            }});
     }
+
+
+
 }
